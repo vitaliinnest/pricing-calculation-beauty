563db99937bdf1346fd4dd5a90a3564458df5476
vitaliinnest <vitaliinnest@gmail.com>
2024-10-13 22:06:05 +0300

    added page skeleton

---
+++ components/ThemedPage.tsx
@@ -0,0 +1,23 @@
+import type { PropsWithChildren } from "react";
+import { ScrollView, StyleSheet } from "react-native";
+
+type Props = PropsWithChildren<{
+  title: string;
+}>;
+
+export default function ThemedPage(props: Props) {
+  return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      {props.children}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1,
+    alignItems: "center",
+    margin: 10,
+    backgroundColor: "red",
+  },
+});
