974152b84b2ca6cc63b58a122426bb1651aa75e3
vitaliinnest <vitaliinnest@gmail.com>
2024-10-19 19:20:53 +0300

    code format & code cleanup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -30,8 +30,16 @@ export default function RootLayout() {
     <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="tool-processing/[id]" options={{ presentation: "modal", headerShown: false }} />
-        <Stack.Screen name="tool-processing/add" options={{ presentation: "modal", headerShown: false }} />
+
+        <Stack.Screen
+          name="tool-processing/[id]"
+          options={{ presentation: "modal", headerShown: false }}
+        />
+        <Stack.Screen
+          name="tool-processing/add"
+          options={{ presentation: "modal", headerShown: false }}
+        />
+
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
