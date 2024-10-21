f9b6875d910f3f82f84c7ceef0840671bfdacb16
vitaliinnest <vitaliinnest@gmail.com>
2024-10-21 17:55:35 +0300

    Refactor toolProcessingCalculators to use ToolProcessingFormValues instead of ToolProcessing

--- calculators/toolProcessingCalculators.ts
+++ calculators/toolProcessingCalculators.ts
@@ -1,17 +1,27 @@
-import { ToolProcessing } from "@/stores/toolProcessingStore";
+import { ToolProcessingFormValues } from "@/stores/toolProcessingStore";
 import { roundUpTo2 } from "@/utils";
 
 /** Кількість днів */
-export function calculateDaysAmount(tool: ToolProcessing): number {
+export function calculateDaysAmount(tool: ToolProcessingFormValues): number {
+  if (tool.expenditurePerDay === 0) {
+    return 0;
+  }
   return roundUpTo2(tool.volume / tool.expenditurePerDay);
 }
 
 /** Ціна в день */
-export function calculatePricePerDay(tool: ToolProcessing): number {
-  return roundUpTo2(tool.price / calculateDaysAmount(tool));
+export function calculatePricePerDay(tool: ToolProcessingFormValues): number {
+  const daysAmount = calculateDaysAmount(tool);
+  if (daysAmount === 0) {
+    return 0;
+  }
+  return roundUpTo2(tool.price / daysAmount);
 }
 
 /** Витрата на одного клієнта */
-export function calculateExpenditurePerClient(tool: ToolProcessing): number {
+export function calculateExpenditurePerClient(tool: ToolProcessingFormValues): number {
+  if (tool.clientsPerDay === 0) {
+    return 0;
+  }
   return roundUpTo2(calculatePricePerDay(tool) / tool.clientsPerDay);
 }
--- components/pages/ToolProcessingDetailsPage.tsx
+++ components/pages/ToolProcessingDetailsPage.tsx
@@ -1,10 +1,14 @@
 import { ToolProcessing, ToolProcessingFormValues } from "@/stores/toolProcessingStore";
+import { TextInput as TextInputNative, StyleSheet } from "react-native";
+import { inputStyles } from "../inputs/common";
+import { LabellableInput } from "../inputs/LabellableInput";
 import ScrollPage from "../ScrollPage";
 import TextInput from "../inputs/TextInput";
 import { useForm } from "react-hook-form";
 import EuroInput from "../inputs/EuroInput";
 import NumberInput from "../inputs/NumberInput";
 import Button from "../Button";
+import { calculateDaysAmount, calculateExpenditurePerClient, calculatePricePerDay } from "@/calculators/toolProcessingCalculators";
 
 type Props = {
   toolProcessing?: ToolProcessing;
@@ -13,7 +17,7 @@ type Props = {
 };
 
 export default function ToolsProcessingDetailsPage({ toolProcessing, onSubmit, onDelete }: Props) {
-  const { control, handleSubmit } = useForm<ToolProcessingFormValues>({
+  const { control, handleSubmit, getValues } = useForm<ToolProcessingFormValues>({
     defaultValues: {
       name: toolProcessing?.name,
       price: toolProcessing?.price,
@@ -22,7 +26,7 @@ export default function ToolsProcessingDetailsPage({ toolProcessing, onSubmit, o
       clientsPerDay: toolProcessing?.clientsPerDay,
     },
   });
-  
+
   return (
     <ScrollPage>
       <TextInput label="Назва" name="name" control={control} />
@@ -30,6 +34,31 @@ export default function ToolsProcessingDetailsPage({ toolProcessing, onSubmit, o
       <NumberInput label="Обсяг" name="volume" control={control} />
       <NumberInput label="Витрати на день" name="expenditurePerDay" control={control} />
       <NumberInput label="Кількість клієнтів на день" name="clientsPerDay" control={control} />
+        
+      <LabellableInput label="Кількість днів">
+        <TextInputNative
+          readOnly
+          value={calculateDaysAmount(getValues()).toString()}
+          style={[inputStyles.input]}
+        />
+      </LabellableInput>
+      
+      <LabellableInput label="Ціна в день, €">
+        <TextInputNative
+          readOnly
+          value={calculatePricePerDay(getValues()).toString()}
+          style={[inputStyles.input]}
+        />
+      </LabellableInput>
+      
+      <LabellableInput label="Витрата на одного клієнта, €">
+        <TextInputNative
+          readOnly
+          value={calculateExpenditurePerClient(getValues()).toString()}
+          style={[inputStyles.input]}
+        />
+      </LabellableInput>
+
       <Button title="Зберегти" onPress={handleSubmit(onSubmit)} />
       {onDelete && <Button title="Видалити" onPress={onDelete} />}
     </ScrollPage>
