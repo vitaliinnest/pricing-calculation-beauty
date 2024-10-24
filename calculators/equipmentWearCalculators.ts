d79f73a854e5bc4203df2ccc79dc188dd5f386f4
vitaliinnest <vitaliinnest@gmail.com>
2024-10-24 20:49:55 +0300

    added equipmentWearCalculators

---
+++ calculators/equipmentWearCalculators.ts
@@ -0,0 +1,39 @@
+import { EquipmentWearFormValues } from "@/stores/equipmentWearStore";
+import { roundUpTo2 } from "@/utils";
+
+/** Ціна в день */
+export function calculatePricePerDay(
+  equipment: EquipmentWearFormValues
+): number {
+  if (!equipment?.serviceLifeInDays) {
+    return 0;
+  }
+  return roundUpTo2(equipment.price / equipment.serviceLifeInDays);
+}
+
+/** Ціна на одного клієнта */
+export function calculatePricePerClient(
+  equipment: EquipmentWearFormValues,
+  averageClientsPerDay: number
+): number {
+  if (averageClientsPerDay === 0) {
+    return 0;
+  }
+  return roundUpTo2(calculatePricePerDay(equipment) / averageClientsPerDay);
+}
+
+/** Сумарна ціна зносу обладнання */
+export function calculateTotalPricePerClient(
+  equipmentWears: EquipmentWearFormValues[],
+  averageClientsPerDay: number
+): number {
+  if (!equipmentWears?.length) {
+    return 0;
+  }
+
+  return equipmentWears.reduce(
+    (acc, equipmentWear) =>
+      acc + calculatePricePerClient(equipmentWear, averageClientsPerDay),
+    0
+  );
+}
