d1888625c95dbd80c7d02a8596c075bd23e47ba2
vitaliinnest <vitaliinnest@gmail.com>
2024-11-03 18:48:40 +0200

    Add price formation store with Zustand for state management

---
+++ stores/priceFormationStore.ts
@@ -0,0 +1,55 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { buildStorage } from "./store";
+
+export interface PriceFormation {
+  // Прибыль с клиента
+  clientProfit: number;
+  // Ваша цена
+  price: number;
+  // Желаемый оборот в месяц
+  expectedMonthlyTurnover: number;
+  // Цена вашей услуги
+  servicePrice: number;
+  // Количество рабочих дней в месяц
+  workingDaysPerMonth: number;
+  // Количество клиентов день
+  clientsNumberPerDay: number;
+}
+
+export type PriceFormationFormValues = Omit<PriceFormation, "id">;
+
+interface PriceFormationStore {
+  priceFormation: PriceFormation;
+  updatePriceFormation: (
+    updatedPriceFormation: PriceFormationFormValues
+  ) => void;
+}
+
+const storage = buildStorage<PriceFormationStore>();
+
+export const usePriceFormationStore = create<PriceFormationStore>()(
+  persist(
+    (set, get) => ({
+      priceFormation: {
+        clientProfit: 0,
+        price: 0,
+        expectedMonthlyTurnover: 0,
+        servicePrice: 0,
+        workingDaysPerMonth: 0,
+        clientsNumberPerDay: 0,
+      },
+
+      updatePriceFormation: (updatedPriceFormation) =>
+        set(() => ({
+          priceFormation: {
+            ...updatedPriceFormation,
+          },
+        })),
+    }),
+    {
+      name: "price-formation-storage",
+      storage,
+    }
+  )
+);
