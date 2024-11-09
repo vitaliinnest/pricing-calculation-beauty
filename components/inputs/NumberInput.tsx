4ab55a93036f37b8bd415681fa7d769ce93ea1d7
vitaliinnest <vitaliinnest@gmail.com>
2024-11-09 17:54:57 +0200

    Refactor NumberInput to enhance value handling and input sanitization

--- components/inputs/NumberInput.tsx
+++ components/inputs/NumberInput.tsx
@@ -2,6 +2,7 @@ import { TextInput as TextInputNative } from "react-native";
 import { useController } from "react-hook-form";
 import { InputProps, inputStyles } from "./common";
 import { LabellableInput } from "./LabellableInput";
+import { useState } from "react";
 
 export default function NumberInput({ label, name, control }: InputProps) {
   const { field } = useController({
@@ -9,25 +10,44 @@ export default function NumberInput({ label, name, control }: InputProps) {
     control,
     defaultValue: 0,
   });
+  const [value, setValue] = useState<string>(field.value.toString());
 
-  const handleChangeText = (text: string) => {
-    const value = parseInt(text);
-    if (!isNaN(value)) {
-      field.onChange(value);
-    } else {
-      field.onChange(0);
-    }
+  const onChangeValue = (text: string) => {
+    const sanitizedText = sanitizeInput(text);
+    setValue(sanitizedText);
+
+    const isFloat = sanitizedText.includes('.') || sanitizedText.includes(',');
+    field.onChange(isFloat ? parseFloat(sanitizedText.replace(',', '.')) : parseInt(sanitizedText, 10));
   };
+
+  const onBlur = () => {
+    setValue(field.value.toString());
+  }
 
   return (
     <LabellableInput label={label} style={inputStyles.inputLabelWrapper}>
       <TextInputNative
-        keyboardType="number-pad"
-        value={field.value.toString()}
-        onChangeText={handleChangeText}
+        keyboardType="decimal-pad"
+        value={value}
+        onChangeText={onChangeValue}
         style={inputStyles.input}
+        onBlur={onBlur}
         selectTextOnFocus
       />
     </LabellableInput>
   );
 }
+function sanitizeInput(text: string) {
+  let sanitizedText = text.replace(/[^0-9.,]/g, '');
+
+  if (sanitizedText.startsWith('0') && sanitizedText.length > 1 && !sanitizedText.startsWith('0.') && !sanitizedText.startsWith('0,')) {
+    sanitizedText = sanitizedText.replace(/^0+/, '');
+  }
+
+  const parts = sanitizedText.split(/[.,]/);
+  if (parts.length > 2) {
+    sanitizedText = parts[0] + '.' + parts.slice(1).join('');
+  }
+  return sanitizedText;
+}
+
