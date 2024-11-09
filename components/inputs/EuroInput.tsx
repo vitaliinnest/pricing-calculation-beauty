202635f8fa0064997932ced3eb5f506a472d30a8
vitaliinnest <vitaliinnest@gmail.com>
2024-11-09 17:45:57 +0200

    Refactor EuroInput to improve value formatting and simplify onChange logic

--- components/inputs/EuroInput.tsx
+++ components/inputs/EuroInput.tsx
@@ -13,31 +13,22 @@ export default function EuroInput({ label, name, control }: InputProps) {
   const [value, setValue] = useState<string>(field.value.toFixed(2));
 
   const onChangeValue = (text: string) => {
-    let formattedText = text.replace(/[^0-9.,]/g, '').replace(/,/g, '.');
-    const parts = formattedText.split('.');
-    if (parts.length > 2) {
-      formattedText = parts[0] + '.' + parts.slice(1).join('');
-    }
-    if (parts[1] && parts[1].length > 2) {
-      formattedText = parts[0] + '.' + parts[1].substring(0, 2);
-    }
-    if (parts[0].startsWith('0') && parts[0].length > 1) {
-      parts[0] = parts[0].replace(/^0+/, '');
-      formattedText = parts.join('.');
-    }
+    let formattedText = formatText(text);
     setValue(formattedText);
-    
+
     const floatValue = formattedText ? parseFloat(formattedText) : 0;
     field.onChange(floatValue);
   };
-  
+
   const onBlur = () => {
-    setValue(parseFloat(value).toFixed(2));
+    setValue(field.value.toFixed(2));
   };
 
-  console.log(field.value);
   return (
-    <LabellableInput label={label + ", €"} style={inputStyles.inputLabelWrapper}>
+    <LabellableInput
+      label={label + ", €"}
+      style={inputStyles.inputLabelWrapper}
+    >
       <TextInputNative
         keyboardType="decimal-pad"
         value={value}
@@ -48,4 +39,20 @@ export default function EuroInput({ label, name, control }: InputProps) {
       />
     </LabellableInput>
   );
+}
+
+function formatText(text: string) {
+  let formattedText = text.replace(/[^0-9.,]/g, "").replace(/,/g, ".");
+  const parts = formattedText.split(".");
+  if (parts.length > 2) {
+    formattedText = parts[0] + "." + parts.slice(1).join("");
+  }
+  if (parts[1] && parts[1].length > 2) {
+    formattedText = parts[0] + "." + parts[1].substring(0, 2);
+  }
+  if (parts[0].startsWith("0") && parts[0].length > 1) {
+    parts[0] = parts[0].replace(/^0+/, "");
+    formattedText = parts.join(".");
+  }
+  return formattedText;
 }
