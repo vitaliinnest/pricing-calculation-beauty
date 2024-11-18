df450fb9d8e2b55a6141ffd8455d0a3a4c414c05
vitaliinnest <vitaliinnest@gmail.com>
2024-11-18 19:41:54 +0200

    Add total price display to expenses list items using KeyValueTable

--- components/pages/ExpensesListPage.tsx
+++ components/pages/ExpensesListPage.tsx
@@ -3,17 +3,27 @@ import { Expense, useExpenseStore } from "@/stores/expenseStore";
 import { useRouter } from "expo-router";
 import AppendableList from "../AppendableList";
 import ListItem from "../ListItem";
+import KeyValueTable from "../KeyValueTable";
+import { useTranslation } from "react-i18next";
+import { calculateTotalPrice } from "@/calculators/expenseCalculators";
 
 export default function ExpensesListPage() {
   const { expenses } = useExpenseStore();
   const router = useRouter();
+  const { t } = useTranslation("expenses");
 
   const renderItem: ListRenderItem<Expense> = ({ item, index }) => (
     <ListItem
       index={index}
       title={item.name}
       onPress={() => router.push(`/expense/${item.id}`)}
-    />
+    >
+      <KeyValueTable
+        data={[
+          [t("totalPrice"), `${calculateTotalPrice(item)} €`],
+        ]}
+      />
+    </ListItem>
   );
 
   return (
